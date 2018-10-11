2dcc10b520e13edf0676212db01f47f18f5ceec8
brisgiwp1 <43769855+brisgiwp1@users.noreply.github.com>
2018-10-12 00:00:08 +0700

    Add files via upload

---
+++ tugasBAB4/program2.cpp
@@ -0,0 +1,18 @@
+void SelectionSort(int Array[], const int Size){
+	int i,j,smallest,temp;
+	for(i=0;i<Size;i++)
+	{
+		smallest=i;
+		for(j=i;j<Size;j++)			{
+			if(Array[smallest]>Array[j])
+			{
+			smallest=j;
+			}
+		}
+		temp=Array[i];
+		Array[i]=Array[smallest];
+		Array[smallest]=temp;
+	}
+	return;
+
+}
